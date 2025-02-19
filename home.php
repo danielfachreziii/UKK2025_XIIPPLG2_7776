830ad45f41c40daaf1cdbfb8d14f3b0962f49511
danielfachreziii <danielfachrezii@gmail.com>
2025-02-19 14:00:57 +0700

    Progres hari 3
    progress

---
+++ home.php
@@ -0,0 +1,28 @@
+<<div class="container-fluid px-4">
+                        <h1 class="mt-4">My To-Do List</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">List</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-xl-6 col-md-6">
+                                <div class="card bg-info text-white mb-4">
+                                    <div class="card-body"><?php echo mysqli_num_rows(mysqli_query($koneksi, "SELECT*FROM categories")); ?> Categories</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="?page=categories">View Details</a>
+                                        <div class="small text-dark"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-md-6">
+                                <div class="card text-white mb-4" style="background-color: orange">
+                                    <div class="card-body"><?php echo mysqli_num_rows(mysqli_query($koneksi, "SELECT*FROM tasks")); ?> Tasks</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="?page=tasks">View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                    
+                        
+                        </div>
+                    </div>
